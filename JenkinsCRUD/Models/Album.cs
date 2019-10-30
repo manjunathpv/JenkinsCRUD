b06fea1f2fd72447d55b938160b7e830706c9ef8
manjunathdev <manjunath.pv@psiog.com>
2019-10-30 14:36:40 +0530

    Add project files.

---
+++ JenkinsCRUD/Models/Album.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JenkinsCRUD.Models
+{
+    public class Album
+    {
+        private MusicContext context;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ArtistName { get; set; }
+
+        public int Price { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
---
+++ JenkinsCRUD/Models/MusicContext.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JenkinsCRUD.Models
+{
+    public class MusicContext
+    {
+        public string ConnectionString { get; set; }
+
+        public MusicContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<Album> GetAllAlbums()
+        {
+            List<Album> list = new List<Album>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from Album where id < 10", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Album()
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Name = reader["Name"].ToString(),
+                            ArtistName = reader["ArtistName"].ToString(),
+                            Price = Convert.ToInt32(reader["Price"]),
+                            Genre = reader["genre"].ToString()
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
